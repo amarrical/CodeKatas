4c6d693d7ef1ed2f3e40108703f3fe89a2e3e273
chrisdlangham <chris.langham32@gmail.com>
2015-11-01 18:19:51 -0400

    arabic to roman numberal converter

---
+++ Homework2/roman_numeral_converter.rb
@@ -0,0 +1,43 @@
+class Converter
+
+	def arabic_to_roman(number)
+		if !number.kind_of?(Fixnum) || number < 0
+      return nil
+    end
+
+    result = ""
+		remaining = number
+		if(number == 0)
+			return "nulla"
+    end
+
+    values = getValues
+
+    values.each do |value, letter|
+      while remaining >= value
+        result << letter
+        remaining -= value
+      end
+    end
+
+		return result
+	end
+
+  def getValues
+    {
+      1000 => "M",
+      900 => "CM",
+      500 => "D", 
+      400 => "CD",
+      100 => "C",
+      90 => "XC",
+      50 => "L",
+      40 => "XL",
+      10 => "X",
+      9 => "IX",
+      5 => "V",
+      4 => "IV",
+      1 => "I"
+    }
+  end
+end
---
+++ Homework2/spec/roman_numberal_converter_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+ 
+RSpec.describe Converter do
+
+	before :each do
+		@converter = Converter.new
+	end
+
+	describe "#new" do
+		it "takes zero arguments" do
+			@converter.should be_an_instance_of Converter
+		end
+	end
+
+	describe "#arabic_to_roman" do
+		it "converts properly" do
+			hash = {
+				0 => "nulla",
+				1 => "I",
+				2 => "II",
+				3 => "III",
+				4 => "IV",
+				5 => "V",
+				6 => "VI",
+				7 => "VII",
+				8 => "VIII",
+				9 => "IX",
+				10 => "X",
+				11 => "XI",
+        1066 => "MLXVI",
+        1989 => "MCMLXXXIX",
+        1999 => "MCMXCIX",
+        2015 => "MMXV",
+        3999 => "MMMCMXCIX",
+        399 => "CCCXCIX"
+			}
+
+			hash.each do |key, value|
+				result = @converter.arabic_to_roman(key)
+				expect(result).to eq value
+			end
+		end
+
+		context "Exceptions" do
+			it "returns empty string for negative numbers" do
+				result = @converter.arabic_to_roman(-1)
+				expect(result).to eq nil
+			end
+
+			it "return empty string for strings" do
+				result = @converter.arabic_to_roman("123")
+				expect(result).to eq nil
+			end
+
+			it "return empty string for objects" do
+				result = @converter.arabic_to_roman(Converter.new)
+				expect(result).to eq nil
+			end
+
+      it "return empty string for nil" do
+        result = @converter.arabic_to_roman(nil)
+        expect(result).to eq nil
+      end
+		end
+	end
+end
